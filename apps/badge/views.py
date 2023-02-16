b8afa1b22e528aaa9fa8f887a208830b26f6a1a2
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2023-02-16 13:09:01 -0300

    Api

--- apps/badge/views.py
+++ apps/badge/views.py
@@ -2,27 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import requests
 
-## Listado de criptos ##
-# def users(request):
-#     response = requests.get('http://api.coinlayer.com/list?access_key=787d56808e80ae8ccdc4e01e00a2818e')
-    
-#     if response.status_code == 200:
-#         response_json= response.json()
-#         origi = response_json['crypto']
-#         lista = origi.values()
-      
-          
-#     return render(request, 'badge/badge.html', {'lista':lista})    
- 
-## Tasa de las mismas ##
-def users(request):
-    response = requests.get('http://api.coinlayer.com/live?access_key=787d56808e80ae8ccdc4e01e00a2818e')
-    
-    if response.status_code == 200:
-        response_json= response.json()
-        origi = response_json['rates']
-        tasa = origi.values()
-        moneda = origi.keys()
 
+def users(request):
+    liv_response = requests.get('http://api.coinlayer.com/live?access_key=787d56808e80ae8ccdc4e01e00a2818e')
+    list_response = requests.get('http://api.coinlayer.com/list?access_key=787d56808e80ae8ccdc4e01e00a2818e')
+    
+    if liv_response.status_code and list_response.status_code ==  200:
+
+        liv_json = liv_response.json ()
+        rates = liv_json['rates']
+        tasa = rates.values()
+        moneda = rates.keys()
+
+        list_json = list_response.json ()
+        crypto = list_json['crypto']
+        crypto_values  = crypto.values()
+    
           
-    return render(request, 'badge/badge.html', {'tasa':tasa,'moneda':moneda})     
+    return render(request, 'badge/badge.html',{'tasa':tasa, 'moneda':moneda,'crypto':crypto_values} )     
+ 
