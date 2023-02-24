3cb237e6292d9fb90e77227f915a00028a78b8df
ezkript <107833602+ezkript@users.noreply.github.com>
2023-02-24 03:44:26 -0300

    [Badge(expand/list)]: view fixed

--- apps/badge/views.py
+++ apps/badge/views.py
@@ -5,7 +5,7 @@ import asyncio
 
 async def list(request):
     async with aiohttp.ClientSession() as session2:
-       async with session2.get('http://api.coinlayer.com/list?access_key=ab2978034ee6b835f2e20c4c75a4f51b', ) as respo2:
+       async with session2.get('http://api.coinlayer.com/list?access_key=450eabae533ad6dda52c606e6526c3f7', ) as respo2:
                     if respo2.status  == 200:
                         data2 = await respo2.json()
                         b = data2['crypto']  
@@ -14,7 +14,7 @@ async def list(request):
 
 async def expand(request):
     async with aiohttp.ClientSession() as session3:
-        async with session3.get('http://api.coinlayer.com/live?access_key=ab2978034ee6b835f2e20c4c75a4f51b&expand=1', ) as resp3:
+        async with session3.get('http://api.coinlayer.com/live?access_key=450eabae533ad6dda52c606e6526c3f7&expand=1', ) as resp3:
                     if resp3.status  == 200:
                         data3 = await resp3.json()
                         c = data3['rates']
