a6d5e8f65b4a8445d762f4afd6ff6b710e9aa7ab
Emanuel Manrique <manriqueemanuel@gmail.com>
2023-02-26 17:25:26 -0300

    home option and main.js fixed

--- static/js/main.js
+++ static/js/main.js
@@ -1,121 +1,125 @@
-
 (function() {
-//===== Prealoder
-
-	window.onload = function () {
-		window.setTimeout(fadeout, 500);
-	}
-
-	function fadeout() {
-		document.querySelector('.preloader').style.opacity = '0';
-		document.querySelector('.preloader').style.display = 'none';
-	}
-
-
-    /*=====================================
-    Sticky
-    ======================================= */
-    window.onscroll = function () {
-        var header_navbar = document.querySelector(".navbar-area");
-        var sticky = header_navbar.offsetTop;
-        var logo = document.querySelector('.navbar-brand img');
-
-        if (window.pageYOffset > sticky) {
-            header_navbar.classList.add("sticky");
-            logo.src = '/static/img/logo/logo-2.svg';
-        } else {
-            header_navbar.classList.remove("sticky");
-            logo.src = '/static/img/logo/logo.png';
-        }
-
-
-
-        // show or hide the back-top-top button
-        var backToTo = document.querySelector(".scroll-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
-        }
+    //===== Prealoder
+  
+    window.onload = function() {
+      window.setTimeout(fadeout, 500);
     };
-
-    // for menu scroll 
-    var pageLink = document.querySelectorAll('.page-scroll');
-    
-    pageLink.forEach(elem => {
-        elem.addEventListener('click', e => {
-            e.preventDefault();
-            document.querySelector(elem.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                offsetTop: 1 - 60,
+  
+    function fadeout() {
+      document.querySelector(".preloader").style.opacity = "0";
+      document.querySelector(".preloader").style.display = "none";
+    }
+  
+    /*=====================================
+      Sticky
+    ======================================= */
+    window.onscroll = function() {
+      const header_navbar = document.querySelector(".navbar-area");
+      const sticky = header_navbar.offsetTop;
+      const logo = document.querySelector(".navbar-brand img");
+  
+      if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+        logo.src = "/static/img/logo/logo-2.svg";
+      } else {
+        header_navbar.classList.remove("sticky");
+        logo.src = "/static/img/logo/logo.png";
+      }
+  
+      // show or hide the back-top-top button
+      const backToTop = document.querySelector(".scroll-top");
+      if (backToTop) {
+        if (
+          document.body.scrollTop > 50 ||
+          document.documentElement.scrollTop > 50
+        ) {
+          backToTop.style.display = "flex";
+        } else {
+          backToTop.style.display = "none";
+        }
+      }
+  
+      // for menu scroll
+      const pageLinks = document.querySelectorAll(".page-scroll");
+  
+      pageLinks.forEach((elem) => {
+        elem.addEventListener("click", (e) => {
+          e.preventDefault();
+          document
+            .querySelector(elem.getAttribute("href"))
+            .scrollIntoView({
+              behavior: "smooth",
+              offsetTop: 1 - 60,
             });
         });
-    });
-
-    // section menu active
-	function onScroll(event) {
-		var sections = document.querySelectorAll('.page-scroll');
-		var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-
-		for (var i = 0; i < sections.length; i++) {
-			var currLink = sections[i];
-			var val = currLink.getAttribute('href');
-			var refElement = document.querySelector(val);
-			var scrollTopMinus = scrollPos + 73;
-			if (refElement.offsetTop <= scrollTopMinus && (refElement.offsetTop + refElement.offsetHeight > scrollTopMinus)) {
-				document.querySelector('.page-scroll').classList.remove('active');
-				currLink.classList.add('active');
-			} else {
-				currLink.classList.remove('active');
-			}
-		}
-	};
-
-	window.document.addEventListener('scroll', onScroll);
-
-
-    //===== close navbar-collapse when a  clicked
-    let navbarToggler = document.querySelector(".navbar-toggler");    
-    var navbarCollapse = document.querySelector(".navbar-collapse");
-
-    document.querySelectorAll(".page-scroll").forEach(e =>
+      });
+  
+      // section menu active
+      function onScroll(event) {
+        const sections = document.querySelectorAll(".page-scroll");
+        const scrollPos =
+          window.pageYOffset ||
+          document.documentElement.scrollTop ||
+          document.body.scrollTop;
+  
+        for (let i = 0; i < sections.length; i++) {
+          const currLink = sections[i];
+          const val = currLink.getAttribute("href");
+          const refElement = document.querySelector(val);
+          const scrollTopMinus = scrollPos + 73;
+          if (
+            refElement.offsetTop <= scrollTopMinus &&
+            refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+          ) {
+            document.querySelector(".page-scroll.active").classList.remove("active");
+            currLink.classList.add("active");
+          } else {
+            currLink.classList.remove("active");
+          }
+        }
+      }
+  
+      window.document.addEventListener("scroll", onScroll);
+  
+      //===== close navbar-collapse when a clicked
+      const navbarToggler = document.querySelector(".navbar-toggler");
+      const navbarCollapse = document.querySelector(".navbar-collapse");
+  
+      document.querySelectorAll(".page-scroll").forEach((e) =>
         e.addEventListener("click", () => {
-            navbarToggler.classList.remove("active");
-            navbarCollapse.classList.remove('show')
+          navbarToggler.classList.remove("active");
+          navbarCollapse.classList.remove("show");
         })
-    );
-    navbarToggler.addEventListener('click', function() {
+      );
+  
+      navbarToggler.addEventListener("click", function() {
         navbarToggler.classList.toggle("active");
-    }) 
-
-
-	// WOW active
-    new WOW().init();
-
-
-    // ====== scroll top js
-    function scrollTo(element, to = 0, duration= 1000) {
-
+      });
+  
+      // WOW active
+      new WOW().init();
+  
+      // ====== scroll top js
+      const scrollTo = (element, to = 0, duration = 1000) => {
         const start = element.scrollTop;
         const change = to - start;
         const increment = 20;
         let currentTime = 0;
-
+  
         const animateScroll = (() => {
-
-            currentTime += increment;
-
-            const val = Math.easeInOutQuad(currentTime, start, change, duration);
-
-            element.scrollTop = val;
-
-            if (currentTime < duration) {
-                setTimeout(animateScroll, increment);
-            }
+          currentTime += increment;
+  
+          const val = Math.easeInOutQuad(currentTime, start, change, duration);
+  
+          element.scrollTop = val;
+  
+          if (currentTime < duration) {
+            setTimeout(animateScroll, increment);
+          }
         });
-
+  
         animateScroll();
-    };
+      };
 
     Math.easeInOutQuad = function (t, b, c, d) {
 
